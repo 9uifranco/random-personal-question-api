8f46cf886a811c5e8bdabd4aaa95b3d1f4de9991
Guilherme Franco <9uifranco@gmail.com>
2023-01-12 10:54:48 -0300

    Add question slice

---
+++ frontend/src/features/questions/questionSlice.js
@@ -0,0 +1,19 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+    question: [],
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: ''
+}
+export const questionSlice = createSlice({
+    name: 'question',
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    },
+})
+
+export const { reset } = questionSlice.actions
+export default questionSlice.reducer
